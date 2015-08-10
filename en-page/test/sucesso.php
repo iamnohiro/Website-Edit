05c20f437ff948da19fd24e924ea3e4bc11f8e30
Alen <iamnohiro@gmail.com>
2015-08-10 16:59:49 +0800

    Delete Not needed Page

--- en-page/test/sucesso.php
+++
@@ -1,5 +0,0 @@
-				<div class="thank">
-                       	<strong>Thank You!</strong>
-                        <p>"Your inquiry/information has been successfully submitted"</p>
-					
-                     </div>
